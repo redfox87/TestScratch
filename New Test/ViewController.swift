e3ff033d394a0f0d378087483da20d076fc6e0f8
Thomas Bloss <TSBloss@gmail.com>
2016-06-04 10:47:29 -0400

    test1

--- New Test/ViewController.swift
+++ New Test/ViewController.swift
@@ -10,8 +10,13 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    let this = "that"
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
 
